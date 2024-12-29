01ee57c450a5cd86e0bc3a446201d92ec0d93920
Matthias <148590631+Matthias-dot-com@users.noreply.github.com>
2024-12-29 14:12:49 +0400

    Rolling state control

--- Tenzie Game/src/component/Main.jsx
+++ Tenzie Game/src/component/Main.jsx
@@ -3,14 +3,16 @@ import { useState } from 'react';
 
 export default function Main(){
     function generateAllNewDice(){
-        return Array(10).fill(0).map(()=>Math.ceil(Math.random()*6))
+        // return Array(10).fill({value:0,isHeld:false}).map((obj)=>({...obj,value: Math.ceil(Math.random()*6)})); // potential bug due to reference
+        return Array(10).fill(null).map(()=> ({value:Math.ceil(Math.random()*6),isHeld:false}))
     }
     const [arrayNewDice, setArrayNewDice] = useState(generateAllNewDice());
 
-    const diceElement = arrayNewDice.map(dice => (<Die value={dice}/>))
+    const diceElement = arrayNewDice.map((dice) => <Die value={dice.value}/>)
 
     function rollDice(){
         setArrayNewDice(generateAllNewDice())
+        console.log(diceElement)
     }
     return (
         <main>
